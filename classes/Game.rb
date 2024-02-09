cc42726aafdc21706586cfd20c85c3c68d00929a
maddie <lee.maddie@gmail.com>
2024-02-09 00:05:41 -0500

    Initialize @winning_player as nil to prevent errors

--- classes/Game.rb
+++ classes/Game.rb
@@ -2,7 +2,7 @@ class Game
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_player = 0
-    @winning_player = 0
+    @winning_player = nil
     @game_over = false
   end
 
