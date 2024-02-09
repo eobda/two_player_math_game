d900e8fceae7e8e63366762cec363bc03b87abbd
maddie <lee.maddie@gmail.com>
2024-02-08 20:04:22 -0500

    Add turn code

--- classes/Game.rb
+++ classes/Game.rb
@@ -6,8 +6,20 @@ class Game
   end
 
   def play_game
-    puts "Player 1 lives: #{@players[0].lives}"
-    puts "Player 2 lives: #{@players[1].lives}"
+    puts "Let's play!"
   end
 
+  def play_turn
+    question = Question.new
+    puts "Player #{@current_player + 1}: #{question.ask_question}"
+    print "> "
+    guess = $stdin.gets.chomp.to_i
+
+    if question.correct?(guess)
+      puts "Player #{@current_player + 1}: YES! You are correct."
+    else
+      puts "Player #{@current_player + 1}: Seriously? No!"
+      @players[@current_player].lose_life
+    end
+  end
 end
